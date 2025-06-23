053da31fae3c4f10938226940a17358be990c32b
HusarArgento <lucasmelfi@live.com.ar>
2025-06-22 22:30:12 -0300

    Ordenamos la carga de datos

--- _scripts/Carga.R
+++ _scripts/Carga.R
@@ -1,13 +1,3 @@
----
-title: "Tp_movilidad_cdde2"
-author: "Lucas S. Melfi"
-date: "2025-05-31"
-output: html_document
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
 
 ```{r Librerías}
 library(tidyverse)
@@ -45,22 +35,22 @@ sum(!st_is_valid(barrios_pop))
 
 ```{r Posibilidad 2: Shape}
 td <- tempdir()
-  download.file(url = paste(url_barrios2,
-                            "barrios-populares-badata.zip",
-                            sep = "/"),
-                destfile = fs::path(td, "barrios-populares-badata.zip") )
-  unzip(zipfile = fs::path(td, "barrios-populares-badata.zip"),
-        exdir = td)
-  barrios3 <- read_sf(fs::path(td, "barrios_vulnerables.shp"))
-  rm(td)
+download.file(url = paste(url_barrios2,
+                          "barrios-populares-badata.zip",
+                          sep = "/"),
+              destfile = fs::path(td, "barrios-populares-badata.zip") )
+unzip(zipfile = fs::path(td, "barrios-populares-badata.zip"),
+      exdir = td)
+barrios3 <- read_sf(fs::path(td, "barrios_vulnerables.shp"))
+rm(td)
 st_crs(barrios2)
 sum(!st_is_valid(barrios2))
 
-  
+
 ```
-#Barrios populares 
+#Barrios populares
 Capa que contiene el registro de los denominados barrios poulares de la Ciudad Autonoma de Buenos Aires. Dichos barrios son considerados las zonas precarias y vulnerables.
-#TIPO_ASENT		
+#TIPO_ASENT
 Tipo de registro: Villa, Asentamiento precario, Barrio municipal, Barrio urbanizado, Conjunto habitacional, Núcleo habitacional transitorio. Son categorías definidas según el Ministerio de Desarrollo y Hábitat en Junio de 2020.
 
 
@@ -82,4 +72,32 @@ Este conjunto de datos proporciona información sobre la ubicación, perímetro 
 
 
 
+```{r Carga de datos}
+
+url_bsasdata <- "https://cdn.buenosaires.gob.ar/datosabiertos/datasets/transporte-y-obras-publicas"
+url_premetro <- "https://cdn.buenosaires.gob.ar//datosabiertos/datasets/sbase/premetro"
+
+#Comunas
+comunas_caba <- st_read("https://cdn.buenosaires.gob.ar/datosabiertos/datasets/ministerio-de-educacion/comunas/comunas.geojson")
+
+#Premetro
+lineas_pre <- st_read(paste(url_premetro, "lineas_de_premetro.geojson",
+                            sep = "/") )
+est_pre <- st_read(paste(url_premetro, "estaciones-premetro.csv", sep = "/"),
+                   options = "GEOM_POSSIBLE_NAMES=geometry",
+                   crs = 4326)
+
+reco_pre <- st_read(paste(url_premetro, "recorrido-premetro.csv", sep = "/"),
+                    options = "GEOM_POSSIBLE_NAMES=WKT",
+                    crs = 4326)
+#Subte
+frec_sub <- read.csv2 (paste(url_bsasdata,
+                             "frecuencia-subte","frecuencia_subte.csv",
+                             sep = "/" ) )
+
+est_sub <-  st_read(dsn = "https://cdn.buenosaires.gob.ar/datosabiertos/datasets/sbase/subte-estaciones/estaciones-de-subte.geojson")
+
+bocas_sub <- st_read(dsn = "https://cdn.buenosaires.gob.ar/datosabiertos/datasets/sbase/bocas-subte/bocas-de-subte.geojson") |>
+  select(-matches("lineas_de_"))
+```
 
